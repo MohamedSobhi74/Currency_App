25b00e50c98abf753660a93d0d7cfd088d2f6904
mohamed.sobhy <1234567890>
2023-05-22 12:45:42 +0200

    - update Api key

--- app/src/main/java/com/project/currencyapp/presentation/utils/Constants.kt
+++ app/src/main/java/com/project/currencyapp/presentation/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.project.currencyapp.presentation.utils
 object Constants {
 
     val BASE_URL: String = "https://api.apilayer.com/fixer/"
-    val API_KEY = "2NezIx7wDfqlVFx9Pe1nnqLjhFMm9GXu";
+    val API_KEY = "vegXzQBScZRfIdx5W62r5IjGRVbmdQhm"
 
 }
